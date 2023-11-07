c07f07c6a1c4372a3257181981e84487cad9628b
alan <alanhuang@moneydj.com>
2023-11-07 15:00:30 +0800

    改寫輪播

--- components/carousel/carousel.tsx
+++ components/carousel/carousel.tsx
@@ -6,11 +6,11 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 export default function Carousel({ allPostsData }: { allPostsData: [{ id: string, name: string }]; }) {
     const carouselItemRef = useRef(null);
     const carouselDotRef = useRef(null);
-    let intervalTime = 3000;
-    let carouselIndex = 0;
-    let intervalCarousel;
-    let intervalProcess;
-    let time = 0;
+    const [intervalTime, setIntervalTime] = useState(3000);
+    const [carouselIndex, setCarouselIndex] = useState(0);
+    const [intervalCarousel, setIntervalCarousel] = useState(null);
+    const [intervalProcess, setIntervalProcess] = useState(null);
+    const [time, setTime] = useState(0);
 
     useEffect(() => {
         let lastImg = carouselItemRef.current.lastElementChild.cloneNode(true);
@@ -23,29 +23,29 @@ export default function Carousel({ allPostsData }: { allPostsData: [{ id: string
         carouselDotRef.current.querySelector('.carouselActive').firstElementChild.style.transition = `${intervalTime / 1000}s linear`
         carouselDotRef.current.querySelector('.carouselActive').firstElementChild.style.width = '100%';
 
-        moveTo(0);
-        intervalCarousel = carouselInterval();
+        nextCarouselControl();
 
         return () => {
-            clearInterval(intervalCarousel);
-            clearInterval(intervalProcess);
+            carouselEnter();
         }
     }, [])
-
-    const carouselInterval = () => {
-        return setInterval(() => {
-            nextCarouselControl();
-        }, intervalTime)
-    }
 
     const carouselEnter = () => {
         clearInterval(intervalCarousel);
         clearInterval(intervalProcess);
+        setIntervalCarousel(null);
+        setIntervalProcess(null)
     }
 
     const carouselLeave = () => {
+        clearInterval(intervalProcess);
+        setIntervalProcess(null)
         moveTo(carouselIndex, true);
     }
+
+    useEffect(() => {
+        moveTo(carouselIndex);
+    }, [carouselIndex])
 
     const moveTo = (index: number, isReEnter: boolean = false) => {
         let carouselActive = carouselDotRef.current.querySelector('.carouselActive');
@@ -57,50 +57,48 @@ export default function Carousel({ allPostsData }: { allPostsData: [{ id: string
         if (!isReEnter) {
             carouselActive.firstElementChild.style.transition = 'none'
             carouselActive.firstElementChild.style.width = '0%';
-            time = 0;
+            setTime(0);
         }
         carouselActive.classList.remove('carouselActive');
         elementLi.classList.add('carouselActive');
 
         clearInterval(intervalProcess);
-        intervalProcess = setInterval(() => {
-            time += (100 / intervalTime) * 100;
-            elementLi.querySelector('div').style.width = `${time}%`;
-            if (time > 100) {
-                if (isReEnter) {
-                    nextCarouselControl();
-                    intervalCarousel = carouselInterval();
-                } else {
+        const processInterval = setInterval(() => {
+            setTime((prev) => {
+                const newTime = prev + (100 / intervalTime) * 100;
+                elementLi.querySelector('div').style.width = `${newTime}%`;
+                if (newTime > 100) {
                     nextCarouselControl();
                 }
-            }
+                return newTime;
+            });
         }, 100)
 
-        carouselIndex = index;
+        setIntervalProcess(processInterval);
     }
 
     const prevCarouselControl = () => {
-        if (carouselIndex === 0) {
-            carouselItemRef.current.style.transition = 'none';
-            carouselItemRef.current.style.transform = `translateX(-${allPostsData.length * 100}%)`;
-            setTimeout(() => {
-                moveTo(allPostsData.length - 1)
-            }, 50)
-        } else {
-            moveTo(carouselIndex - 1)
-        }
+        setCarouselIndex((prevIndex) => {
+            if (prevIndex === 0) {
+                carouselItemRef.current.style.transition = 'none';
+                carouselItemRef.current.style.transform = `translateX(-${allPostsData.length * 100}%)`;
+                return allPostsData.length - 1;
+            } else {
+                return prevIndex - 1;
+            }
+        })
     }
 
     const nextCarouselControl = () => {
-        if (carouselIndex === allPostsData.length - 1) {
-            carouselItemRef.current.style.transition = 'none';
-            carouselItemRef.current.style.transform = `translateX(100%)`;
-            setTimeout(() => {
-                moveTo(0)
-            }, 50)
-        } else {
-            moveTo(carouselIndex + 1)
-        }
+        setCarouselIndex((prevIndex) => {
+            if (prevIndex === allPostsData.length - 1) {
+                carouselItemRef.current.style.transition = 'none';
+                carouselItemRef.current.style.transform = `translateX(100%)`;
+                return 0;
+            } else {
+                return prevIndex + 1;
+            }
+        })
     }
 
     const carouselDotClick = (index) => {
